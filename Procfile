85034d17d6366ac4a0c0317e48c6b033ff2df395
Jesman Dzimba <jesmandzimba@Jesmans-MacBook-Pro.local>
2021-05-03 15:30:13 +0200

    Added .env example file, Profile, Requirements and runtime files for deplayment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ckmart.wsgi --log-file -
--- ckmart/settings.py
+++ ckmart/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +25,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a+k=9y&y&%*ajyh6-=8q(##gb6kkafyfuyb%fyx+v9)@g_0s17'
+SECRET_KEY = env('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS_ENV = env('ALLOWED_HOSTS')
+if ALLOWED_HOSTS_ENV:
+    ALLOWED_HOSTS.extend(ALLOWED_HOSTS_ENV.split(','))
 
 
 # Application definition
